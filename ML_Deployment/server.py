81b3984810a277766dee1c4b852c09bddc840820
Saurabh Suresh Powar <66636289+Spnetic-5@users.noreply.github.com>
2021-05-06 01:16:00 +0530

    added books/resources

---
+++ ML_Deployment/server.py
@@ -0,0 +1,35 @@
+import pickle
+import logging
+from fastapi import FastAPI
+from model.iris import Iris
+
+app = FastAPI(title="Iris Classifier API", description="API for Iris classification using ML", version="1.0")
+
+# Initialize logging
+my_logger = logging.getLogger()
+my_logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG, filename='logs.log')
+
+model = None
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model = pickle.load(open("models/model.pkl", "rb"))
+
+@app.post("/api", tags=["prediction"])
+async def get_predictions(iris: Iris):
+    try:
+        data = dict(iris)['data']
+        print(data)
+        iris_types = {
+            0: 'setosa',
+            1: 'versicolor',
+            2: 'virginica'
+        }
+        prediction = list(map(lambda x: iris_types[x], model.predict(data).tolist()))
+        log_proba = model.predict_log_proba(data).tolist()
+        return {"prediction": prediction, "log_proba": log_proba}
+    except:
+        my_logger.error("Something went wrong!")
+        return {"prediction": "error"}
